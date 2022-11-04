c8069d17030e719b97a812cc787784b5f9772b3b
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-03 22:19:01 -0300

    Add Routes

--- App.tsx
+++ App.tsx
@@ -9,12 +9,10 @@ import {
 import { THEME } from "./src/styles/theme";
 
 import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
-import { New } from "./src/screens/New";
-import { Find } from "./src/screens/Find";
-import { Pools } from "./src/screens/Pools";
 
 import { AuthContextProvider } from "./src/contexts/AuthContext";
+
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +29,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
